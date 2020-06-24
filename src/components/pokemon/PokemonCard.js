4e1e915591e6bcec18a391e0cc910c0c568a1928
Danny <dannyisapple@gmail.com>
2020-06-24 15:54:36 -0400

    fixed

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,17 +1,5 @@
 import React, { Component } from "react";
 
-function importAll(r) {
-  return r.keys().map(r);
-}
-
-const images = importAll(
-  require.context(
-    "./sprites/sprites/sprites/pokemon/",
-    false,
-    /\.(png|jpe?g|svg)$/
-  )
-);
-
 export default class PokemonCard extends Component {
   state = {
     name: "",
@@ -24,8 +12,6 @@ export default class PokemonCard extends Component {
     const url = this.props.url;
     const pokemonIndex = url.split("/")[url.split("/").length - 2];
     const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png?raw=true`;
-    // const imageUrl = `./sprites/sprites/spirtes/${pokemonIndex}.png`;
-    // const imageUrl = images[`${pokemonIndex}`];
 
     this.setState({
       name,
